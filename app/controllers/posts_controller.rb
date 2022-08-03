8d5e06cd2ca1d15ba17945cd99aa5778f98aeb18
triznylaras <triznylaras@gmail.com>
2022-08-03 13:30:54 +0700

    add index, new, create methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,23 @@
 class PostsController < ApplicationController
+  # GET /posts
+  # GET /posts.json
   def index
+    @post = Post.all
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
